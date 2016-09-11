338c64cbf47877fc755f347f06bc7ab9b1b219b3
tiernanmartin <tiernan.martin@gmail.comgit config --listgit config --listgit congif --listgit config --global user.email tiernan.martin@gmail.com>
2016-09-11 09:47:38 -0700

    example showing `leafletProxy` problem (submitted to Shiny Google Group)

--- inst/examples/tmp/app.R
+++ inst/examples/tmp/app.R
@@ -7,8 +7,12 @@ quakeMapUI <- function(id){
 
         tagList(
                 bootstrapPage(
-                tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
-                leafletOutput(ns("map"), width = "100%", height = "100%"),
+                        fluidRow(
+                                tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
+                                leafletOutput(ns("map"), width = "100%", height = "100%"),
+                        ),
+
+
                 absolutePanel(top = 10, right = 10,
                               sliderInput(ns("range"), "Magnitudes", min(quakes$mag), max(quakes$mag),
                                           value = range(quakes$mag), step = 0.1
@@ -53,7 +57,7 @@ quakeMap <- function(input, output, session, data_rx, id_copy){
         observe({
                 pal <- colorpal()
 
-                leafletProxy("map", data = filteredData()) %>%
+                leafletProxy(session$ns("map"), data = filteredData()) %>%
                         clearShapes() %>%
                         clearControls() %>%
                         addCircles(radius = ~10^mag/10, weight = 1, color = "#777777",
